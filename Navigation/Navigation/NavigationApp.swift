6988c5d6913000d30a6860a04ff83d2ab7bebd28
Shaileepatel2099 <shaileepatel2001@gmail.com>
2023-03-28 09:08:34 -0400

    start
    design and code

---
+++ Navigation/Navigation/NavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationApp.swift
+//  Navigation
+//
+//  Created by Parth Patel on 2023-03-28.
+//
+
+import SwiftUI
+
+@main
+struct NavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
